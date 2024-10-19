8d8a1cde6b859807e44be57dd3138f17fabf0918
Camelle <baloiscamelle@gmail.com>
2024-10-19 18:43:56 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,8 @@
+// Favorite Singers
+let favSingers = ["Taylor Swift", "Dionela", "James Arthur"];
+console.log("My first favorite singer is", favSingers[0]);
+
+// Favorite Numbers
+let favNumbers = ["22", "25", "03", "24"];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr);
