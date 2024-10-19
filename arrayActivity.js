003911ec4825d6ff6fdb15b9e134486613222d9b
Camelle <baloiscamelle@gmail.com>
2024-10-19 18:48:30 +0800

    git commit -m "intialcommit"

--- arrayActivity.js
+++ arrayActivity.js
@@ -3,6 +3,6 @@ let favSingers = ["Taylor Swift", "Dionela", "James Arthur"];
 console.log("My first favorite singer is", favSingers[0]);
 
 // Favorite Numbers
-let favNumbers = ["22", "25", "03", "24"];
+let favNumbers = ["22", "25", "03", "21"];
 let mixedArr = ["string", ["otherarray"], 123, true];
 console.log(mixedArr);
